f22fac1e9d1fca78f961d254eb96ce92d0195fb4
YoungSmoke <71344549+SoupCS@users.noreply.github.com>
2025-02-27 11:07:45 +0700

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -5,33 +5,7 @@ console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 
-let count = 0;
 
-function getRandomNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
-
-game();
-
-function game() {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    let int = getRandomNumber(1,100);
-    let correct = int % 2 == 0 ? 'yes' : 'no';
-    console.log(`Question: ${int}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer == correct) {
-        console.log('Correct!');
-        ++count;
-        if (count === 3) {
-            console.log(`Congratulations, ${name}!`);
-            ;
-        } else {
-            game();
-        }
-    } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-    }
-}
 
 
 
